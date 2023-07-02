f00a118ad5b918f08b0fb5c32ec53854d45a02a9
miiki00 <mikiasabiy16@gmail.com>
2023-07-02 04:30:44 -0400

    add: file_storage.py: a class that handles file storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+"""
+file_storage:
+    This module holds the defination of the class FileStorage
+    which handles the storage of objects.
+"""
+
+import json
+
+
+class FileStorage:
+    """
+    FileStorage: handles object storage persistency
+
+    Attributes:
+        __file_path (str): path to the json file holding the objects
+                           json representation.
+       __objects (dict): Dictionary holding the objects.
+    """
+
+    __objects = {}
+    __file_path = 'storage.json'
+
+    def all(self):
+        """
+        all(self):
+            Returns dictionary of the objects stored.
+
+        Return:
+            Dictionry of the the stored objects.
+        """
+        return (FileStorage.__objects)
+
+    def new(self, obj):
+        """
+        new(self, obj):
+            Inserts new object to the collection of objects.
+
+        Args:
+            obj: The object to be stored.
+
+        Return:
+            None.
+        """
+        FileStorage.__objects[f'{type(obj).__name__}.{obj.id}'] = obj
+
+    def save(self):
+        """
+        save(self):
+            Saves the objects collection dictionary in to a file
+            after converting it to a json representation.
+
+        Return:
+            None.
+        """
+        with open(FileStorage.__file_path, 'w') as file:
+            to_serialize = FileStorage.__objects.copy()
+            for key in to_serialize.keys():
+                to_serialize[key] = to_serialize[key].to_dict()
+            file.write(json.dumps(to_serialize))
+
+    def reload(self):
+        """
+        reload(self):
+            Reloads the objects from the last saved file that
+            that contains the json representation of the objects.
+        """
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                json_str = file.read()
+                if len(json_str) != 0:
+                    _raw = json.loads(json_str)
+                    for key in _raw.keys():
+                        if _raw[key]['__class__'] == 'BaseModel':
+                            from models.base_model import BaseModel
+                            _raw[key] = BaseModel(**_raw[key])
+                    FileStorage.__objects = _raw
+        except FileNotFoundError:
+            pass
