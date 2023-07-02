fa252fa7d9274d2c6dfad59a40a9b5c25a531fe6
miiki00 <mikiasabiy16@gmail.com>
2023-07-02 14:15:05 -0400

    add: place.py: defination of Place class

---
+++ models/place.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+place:
+    contains the defination of the class Place.
+"""
+
+
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """
+    Place:
+        Class defination for Place object.
+    """
+
+    city_id = ''
+    user_id = ''
+    name = ''
+    description = ''
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(**kwargs)
